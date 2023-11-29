a55527e5f4635237a29f8d07f1cd562fa964319d
Ramzi <ramzi@dataddo.com>
2023-11-29 23:21:37 +0100

    Introduction Image UI Fix

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -26,21 +26,52 @@
       </div>
       <div class="absolute bottom-0 w-full">
         <div class="relative w-full flex justify-center max-lg:px-4 lg:scale-75 xl:scale-100 duration-500 max-w-screen-xl mx-auto">
-          <img src="~/assets/images/splash-profile.png" alt="Ahmed Ramzi" width="800" height="600" class="lg:absolute z-0 bottom-0" />
-          <SocialMedia class="absolute max-sm:hidden right-8 md:top-24 lg:-top-96" />
+          <img src="~/assets/images/splash-profile.png" alt="Ahmed Ramzi" width="800" height="600" class="lg:absolute my-image z-0 bottom-0" />
+          <SocialMedia class="social absolute right-4 md:right-8 -top-44 md:top-24 lg:-top-96" />
         </div>
       </div>
     </main>
   </div>
 </template>
 
+<script setup lang="ts">
+const height = ref(0)
+let observer: ResizeObserver | null = null
+onBeforeMount(() => {
+  observer = new ResizeObserver((entries) => {
+    height.value = entries[0].contentRect.height
+  })
+})
+onMounted(() => {
+  const element: HTMLElement | null = document.getElementById("__nuxt")
+  if (element && observer) observer.observe(element)
+})
+onUnmounted(() => {
+  if (observer) {
+    observer.disconnect()
+  }
+})
+</script>
+
 <style scoped>
 main {
   @apply pt-16 pb-3 lg:py-24 relative;
 }
 
+@media (max-height: 640px) {
+  .social {
+    @apply hidden;
+  }
+}
+@media (min-height: 650px) {
+  .my-image {
+    @apply max-sm:scale-150;
+  }
+}
+
 @media (max-width: 1023px) {
   main {
+    @apply overflow-hidden;
     height: calc(100vh - 83px);
   }
 }
